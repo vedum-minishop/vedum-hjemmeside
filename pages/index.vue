0dcd206c2107f034a7c19f3114175731e92df909
Robert Axelsen <robert.axelsen@ebcont.com>
2020-04-04 10:49:11 +0200

    make further form adjustments for netlify

--- pages/index.vue
+++ pages/index.vue
@@ -176,8 +176,7 @@
           </v-responsive>
 
           <v-theme-provider light>
-            <v-form name="kontakt" ref="form" v-model="valid" action="/takk" method="post" lazy-validation data-netlify="true" data-netlify-honepot="bot-field">
-              <input type="hidden" name="form-name" value="contactus" form-name="kontakt" />
+            <v-form name="kontaktskjema" ref="form" v-model="valid" action="/takk/" method="post" lazy-validation data-netlify="true">
               <v-row>
                 <v-col cols="12">
                   <v-text-field
